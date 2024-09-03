07fc77ab4d87f0f08fcaafc2bf1cbc9a5aabc9a6
LucasZanella <lucasza.privado@gmail.com>
2024-09-03 19:32:40 -0300

    teste

--- src/main/java/com/example/jogodavelha/JogoApplication.java
+++ src/main/java/com/example/jogodavelha/JogoApplication.java
@@ -11,7 +11,7 @@ public class JogoApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(JogoApplication.class.getResource("menu-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 375, 442);
+        Scene scene = new Scene(fxmlLoader.load(), 375, 216);
         stage.setTitle("Jogo da Velha");
         stage.setScene(scene);
         stage.show();
